956afc5e632fd43b543ba129627e1b1fb45fadc6
pacanada <pereirapcanada@gmail.com>
2024-11-28 16:37:15 +0100

    some dev

--- project-euler/src/bin/21_amicable_numbers.rs
+++ project-euler/src/bin/21_amicable_numbers.rs
@@ -1,19 +1,30 @@
 use std::collections::HashMap;
 
-use num::integer::Roots;
 use project_euler::utils;
 
 fn approach_1(n: u32) -> u32 {
     // brute force
-    let mut m:HashMap<u32, u32>  = HashMap::new();
+    let mut v: Vec<u32> = Vec::with_capacity(n as usize);
+    // to start with the right index, arrays cannot be used bc n needs to be known at compiled time
+    v.push(0);
     for i in 1..=n {
         let divisors: Vec<u32> = (1..=i/2+1).filter(|x| i%x == 0).collect();
-        m.insert(i, divisors.iter().sum());
-        println!("{i}: {:?}", divisors);
+        v.push(divisors.iter().sum());
+       // println!("{i}: {:?}, sum: {}", divisors, divisors.iter().sum::<u32>());
     }
-    println!("{:?}", m);
+    let mut amicable_numbers: Vec<u32> = Vec::new();
+    for (index, value) in v.iter().enumerate() {
+        if *value as usize > v.len() {continue;}
+        if (v[*value as usize]==index as u32) & (*value!=index as u32){
+            amicable_numbers.push(*value);
+
+        }
+
+    }
+    //println!("{:?}", v);
+    println!("{:?}", amicable_numbers);
     
-    0
+    amicable_numbers.iter().sum()
     }
 fn approach_2(n: u32) -> u32 {
     fn sum_of_divisors(n: u32)->u32{
@@ -29,7 +40,8 @@ fn approach_2(n: u32) -> u32 {
     
     fn main() {
     
-        let n = 12;
+        let n = 10000;
         let sol = approach_1(n);
-        println!(" {n}  is {sol}");
+        // or even better: https://mathschallenge.net/index.php?section=faq&ref=number/sum_of_divisors
+        println!("Sum of all amicable numbers under {n}  is {sol}");
     }
